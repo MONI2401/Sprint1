b7adfb1e156a62850c38eb74228cde1f2aa408b4
VivekRaja2000 <vivek062000@gmail.com>
2021-04-02 13:06:37 +0530

    Add

--- cars24/src/main/java/com/cg/cars/entities/Appointment.java
+++ cars24/src/main/java/com/cg/cars/entities/Appointment.java
@@ -18,5 +18,89 @@ public class Appointment {
 	private LocalTime preferredTime;
 	private Customer customer;
 	private Payment payment;
+	/**
+	 * @return the appointmentId
+	 */
+	public long getAppointmentId() {
+		return appointmentId;
+	}
+	/**
+	 * @param appointmentId the appointmentId to set
+	 */
+	public void setAppointmentId(long appointmentId) {
+		this.appointmentId = appointmentId;
+	}
+	/**
+	 * @return the location
+	 */
+	public String getLocation() {
+		return location;
+	}
+	/**
+	 * @param location the location to set
+	 */
+	public void setLocation(String location) {
+		this.location = location;
+	}
+	/**
+	 * @return the inspectionType
+	 */
+	public String getInspectionType() {
+		return inspectionType;
+	}
+	/**
+	 * @param inspectionType the inspectionType to set
+	 */
+	public void setInspectionType(String inspectionType) {
+		this.inspectionType = inspectionType;
+	}
+	/**
+	 * @return the preferredDate
+	 */
+	public LocalDate getPreferredDate() {
+		return preferredDate;
+	}
+	/**
+	 * @param preferredDate the preferredDate to set
+	 */
+	public void setPreferredDate(LocalDate preferredDate) {
+		this.preferredDate = preferredDate;
+	}
+	/**
+	 * @return the preferredTime
+	 */
+	public LocalTime getPreferredTime() {
+		return preferredTime;
+	}
+	/**
+	 * @param preferredTime the preferredTime to set
+	 */
+	public void setPreferredTime(LocalTime preferredTime) {
+		this.preferredTime = preferredTime;
+	}
+	/**
+	 * @return the customer
+	 */
+	public Customer getCustomer() {
+		return customer;
+	}
+	/**
+	 * @param customer the customer to set
+	 */
+	public void setCustomer(Customer customer) {
+		this.customer = customer;
+	}
+	/**
+	 * @return the payment
+	 */
+	public Payment getPayment() {
+		return payment;
+	}
+	/**
+	 * @param payment the payment to set
+	 */
+	public void setPayment(Payment payment) {
+		this.payment = payment;
+	}
 
 }
