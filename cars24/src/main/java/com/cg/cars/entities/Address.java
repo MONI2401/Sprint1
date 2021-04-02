6bc349f21cc2c58fb3f72c90a414f0dd9b2938a5
VivekRaja2000 <vivek062000@gmail.com>
2021-04-02 15:07:54 +0530

    added mappings

--- cars24/src/main/java/com/cg/cars/entities/Address.java
+++ cars24/src/main/java/com/cg/cars/entities/Address.java
@@ -1,11 +1,158 @@
 package com.cg.cars.entities;
 
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.ManyToMany;
+import javax.persistence.JoinColumn;
+import java.util.List;
+
+@Entity
+@Table(name="Address")
 public class Address {
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private int addressId;
+
+	@Column(name="doorno",nullable = false)
 	private String doorNo;
+
+	@Column(name="street",nullable = false)
 	private String street;
+
+	@Column(name="area")
 	private String area;
+
+	@Column(name="city",nullable=false)
 	private String city;
+
+	@Column(name="state",nullable = false)
 	private String state;
+
+	@Column(name="pincode",nullable=false)
 	private int pincode;
+
+	@ManyToMany(targetEntity = Customer.class,fetch = FetchType.EAGER, mappedBy = "userId",cascade = CascadeType.REFRESH)
+	@JoinColumn(name="userId",nullable=false)
+	private List<Customer> customers;
+
+	/**
+	 * @return the addressId
+	 */
+	public int getAddressId() {
+		return addressId;
+	}
+
+	/**
+	 * @param addressId the addressId to set
+	 */
+	public void setAddressId(int addressId) {
+		this.addressId = addressId;
+	}
+
+	/**
+	 * @return the doorNo
+	 */
+	public String getDoorNo() {
+		return doorNo;
+	}
+
+	/**
+	 * @param doorNo the doorNo to set
+	 */
+	public void setDoorNo(String doorNo) {
+		this.doorNo = doorNo;
+	}
+
+	/**
+	 * @return the street
+	 */
+	public String getStreet() {
+		return street;
+	}
+
+	/**
+	 * @param street the street to set
+	 */
+	public void setStreet(String street) {
+		this.street = street;
+	}
+
+	/**
+	 * @return the area
+	 */
+	public String getArea() {
+		return area;
+	}
+
+	/**
+	 * @param area the area to set
+	 */
+	public void setArea(String area) {
+		this.area = area;
+	}
+
+	/**
+	 * @return the city
+	 */
+	public String getCity() {
+		return city;
+	}
+
+	/**
+	 * @param city the city to set
+	 */
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	/**
+	 * @return the state
+	 */
+	public String getState() {
+		return state;
+	}
+
+	/**
+	 * @param state the state to set
+	 */
+	public void setState(String state) {
+		this.state = state;
+	}
+
+	/**
+	 * @return the pincode
+	 */
+	public int getPincode() {
+		return pincode;
+	}
+
+	/**
+	 * @param pincode the pincode to set
+	 */
+	public void setPincode(int pincode) {
+		this.pincode = pincode;
+	}
+
+	/**
+	 * @return the customers
+	 */
+	public List<Customer> getCustomers() {
+		return customers;
+	}
+
+	/**
+	 * @param customers the customers to set
+	 */
+	public void setCustomers(List<Customer> customers) {
+		this.customers = customers;
+	}
+
 
 }
--- cars24/src/main/java/com/cg/cars/entities/Customer.java
+++ cars24/src/main/java/com/cg/cars/entities/Customer.java
@@ -3,19 +3,49 @@ package com.cg.cars.entities;
 import java.time.LocalDate;
 
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.ManyToMany;
+import javax.persistence.JoinColumn;
+import java.util.List;
+import java.util.ArrayList;
+import javax.persistence.JoinTable;
+import java.util.Date;
+
+
 @Entity
+@Table(name="Customer")
 public class Customer {
+	
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private String userId;
+	
+	@Column(name="name",nullable=false,length=50)
 	private String name;
+
+	@Column(name="email",nullable=false,unique=true,length = 255)
 	private String email;
+
+	@Column(name="contactno",nullable=false,unique=true,length = 10)
 	private String contactNo;
-	private  LocalDate dob;
-	private Address address;
+
+	@Column(name="dob",nullable = false)
+	@Temporal(TemporalType.DATE)
+	private  Date dob;
+
+	@ManyToMany(targetEntity = Address.class,fetch = FetchType.EAGER,mappedBy = "addressId",cascade = CascadeType.REFRESH)
+	@JoinColumn(name="addressId",nullable = false)
+	private List<Address> address;
+
+
 	public String getUserId() {
 		return userId;
 	}
@@ -40,20 +70,21 @@ public class Customer {
 	public void setContactNo(String contactNo) {
 		this.contactNo = contactNo;
 	}
-	public LocalDate getDob() {
+	public Date getDob() {
 		return dob;
 	}
-	public void setDob(LocalDate dob) {
+	public void setDob(Date dob) {
 		this.dob = dob;
 	}
-	public Address getAddress() {
-		return address;
-	}
-	public void setAddress(Address address) {
-		this.address = address;
-	}
+	// public Address getAddress() {
+	// 	return address;
+	// }
+	// public void setAddress(Address address) {
+	// 	this.address = address;
+	// }
 	public Customer(String userId, String name, String email, String contactNo, LocalDate dob, Address address) {
 		super();
+		this.address=new ArrayList<Address>();
 		this.userId = userId;
 		this.name = name;
 		this.email = email;
