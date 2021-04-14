b0bb1096908852076de4852bef3ff417d8fb6bcc
Dhivya2907 <81764071+Dhivya2907@users.noreply.github.com>
2021-04-15 00:04:37 +0530

    Update CarDTO.java

--- cars24/src/main/java/com/cg/cars/model/CarDTO.java
+++ cars24/src/main/java/com/cg/cars/model/CarDTO.java
@@ -1,59 +1,79 @@
 package com.cg.cars.model;
 
-import java.util.Date;
+import java.time.LocalDate;
 
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 
 import org.springframework.stereotype.Component;
 
+import com.cg.cars.entities.Car;
+import com.cg.cars.entities.Customer;
+
 @Component
-public class CarDTO {
+public class CarDTO  {
 	@GeneratedValue(strategy = GenerationType.AUTO)
-	private int carId;
+	private long carId;
 	private String brand;
 	private String model;
 	private String variant;
-	private Date registrationYear;
+	private LocalDate registrationYear;
 	private String registrationState;
-	public int getCarId() {
+	private Customer customer;
+
+	public long getCarId() {
 		return carId;
 	}
-	public void setCarId(int carId) {
+
+	public void setCarId(long carId) {
 		this.carId = carId;
 	}
+
 	public String getBrand() {
 		return brand;
 	}
+
 	public void setBrand(String brand) {
 		this.brand = brand;
 	}
+
 	public String getModel() {
 		return model;
 	}
+
 	public void setModel(String model) {
 		this.model = model;
 	}
+
 	public String getVariant() {
 		return variant;
 	}
+
 	public void setVariant(String variant) {
 		this.variant = variant;
 	}
-	public Date getRegistrationYear() {
+
+	public LocalDate getRegistrationYear() {
 		return registrationYear;
 	}
-	public void setRegistrationYear(Date registrationYear) {
+
+	public void setRegistrationYear(LocalDate registrationYear) {
 		this.registrationYear = registrationYear;
 	}
+
 	public String getRegistrationState() {
 		return registrationState;
 	}
-	public void setRegistrationState(String registrationState) {
-		this.registrationState = registrationState;
+
+	@Override
+	public String toString() {
+		return "CarDTO [carId=" + carId + ", brand=" + brand + ", model=" + model + ", variant=" + variant
+				+ ", registrationYear=" + registrationYear + ", registrationState=" + registrationState + ", customer="
+				+ customer + "]";
 	}
-	public CarDTO(int carId, String brand, String model, String variant, Date registrationYear,
-			String registrationState) {
+
+	public CarDTO(long carId, String brand, String model, String variant, LocalDate registrationYear,
+			String registrationState, Customer customers) {
 		super();
 		this.carId = carId;
 		this.brand = brand;
@@ -61,15 +81,25 @@ public class CarDTO {
 		this.variant = variant;
 		this.registrationYear = registrationYear;
 		this.registrationState = registrationState;
-	}
-	public CarDTO() {
-		super();
-		
-	}
-	@Override
-	public String toString() {
-		return "CarDTO [carId=" + carId + ", brand=" + brand + ", model=" + model + ", variant=" + variant
-				+ ", registrationYear=" + registrationYear + ", registrationState=" + registrationState + "]";
+		this.customer = customers;
 	}
 
+	public CarDTO() {
+		super();
+
+	}
+
+	public Customer getCustomer() {
+		return customer;
+	}
+
+	public void setCustomer(Customer customer) {
+		this.customer = customer;
+	}
+
+	public void setRegistrationState(String registrationState) {
+		this.registrationState = registrationState;
+	}
+
+	
 }
