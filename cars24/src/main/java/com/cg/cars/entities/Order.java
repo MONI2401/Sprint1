ba4f9ad416ab1fe7c8238ddfbadd74605db68651
Dhivya2907 <81764071+Dhivya2907@users.noreply.github.com>
2021-04-14 22:59:57 +0530

    Update Order.java

--- cars24/src/main/java/com/cg/cars/entities/Order.java
+++ cars24/src/main/java/com/cg/cars/entities/Order.java
@@ -12,8 +12,7 @@ import javax.persistence.Table;
 
 @Entity
 @Table(name = "Orders")
-public class Order  implements Comparable<Order>
-{
+public class Order  {
 	public Order() {
 		super();
 
@@ -84,57 +83,12 @@ public class Order  implements Comparable<Order>
 	}
 
 	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		long temp;
-		temp = Double.doubleToLongBits(amount);
-		result = prime * result + (int) (temp ^ (temp >>> 32));
-		result = prime * result + ((billingDate == null) ? 0 : billingDate.hashCode());
-		result = prime * result + ((customer == null) ? 0 : customer.hashCode());
-		result = prime * result + (int) (orderId ^ (orderId >>> 32));
-		result = prime * result + ((paymentMethod == null) ? 0 : paymentMethod.hashCode());
-		return result;
+	public String toString() {
+		return "Order [orderId=" + orderId + ", amount=" + amount + ", billingDate=" + billingDate + ", customer="
+				+ customer + ", paymentMethod=" + paymentMethod + "]";
 	}
 
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Order other = (Order) obj;
-		if (Double.doubleToLongBits(amount) != Double.doubleToLongBits(other.amount))
-			return false;
-		if (billingDate == null) {
-			if (other.billingDate != null)
-				return false;
-		} else if (!billingDate.equals(other.billingDate))
-			return false;
-		if (customer == null) {
-			if (other.customer != null)
-				return false;
-		} else if (!customer.equals(other.customer))
-			return false;
-		if (orderId != other.orderId)
-			return false;
-		if (paymentMethod == null) {
-			if (other.paymentMethod != null)
-				return false;
-		} else if (!paymentMethod.equals(other.paymentMethod))
-			return false;
-		return true;
-	}
-
-	@Override
-	public int compareTo(Order o) {
-
-		
-	return (int) (this.orderId - o.orderId + this.amount-o.amount +this.billingDate.compareTo(o.billingDate)+
-			this.customer.compareTo(o.customer)+this.paymentMethod.compareTo(o.paymentMethod));
-
-	}			
+	
+			
 
 }
