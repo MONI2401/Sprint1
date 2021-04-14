d0d520621d864bf9181c0aea5183d8edf6bf743b
MONI2401 <52850693+MONI2401@users.noreply.github.com>
2021-04-14 21:24:45 +0530

    CustomerServiceException updated

--- cars24/src/main/java/com/cg/cars/exception/CustomerServiceException.java
+++ cars24/src/main/java/com/cg/cars/exception/CustomerServiceException.java
@@ -1,10 +1,10 @@
 package com.cg.cars.exception;
 
 @SuppressWarnings("serial")
-public class CustomerServiceException extends RuntimeException {
+public class CustomerServiceException extends Exception {
 
-	public CustomerServiceException(String msg) {
-		super(msg);
+	public CustomerServiceException(String message) {
+		super(message);
+
 	}
-
 }
