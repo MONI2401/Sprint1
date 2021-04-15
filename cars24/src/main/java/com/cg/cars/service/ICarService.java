e85151501724e3fd85709e4438c83097dbc0842b
Dhivya2907 <81764071+Dhivya2907@users.noreply.github.com>
2021-04-15 10:36:23 +0530

    Update ICarService.java

--- cars24/src/main/java/com/cg/cars/service/ICarService.java
+++ cars24/src/main/java/com/cg/cars/service/ICarService.java
@@ -3,19 +3,24 @@ package com.cg.cars.service;
 import java.util.List;
 
 import com.cg.cars.entities.Car;
+import com.cg.cars.exception.CarServiceException;
 import com.cg.cars.model.CarDTO;
 
 public interface ICarService {
-	public CarDTO addCar(Car car);
-	public CarDTO removeCar(int id);
-	public CarDTO updateCar(Car car);
-	public CarDTO getCar(int id);
-	public List<CarDTO> getAllCars();
-	public CarDTO getCarsByCity(String city);
-	public CarDTO getCarsByModel(String model);
-	public CarDTO getCarsByBrand(String brand);
+	public CarDTO addCar(Car car) throws CarServiceException;
 
+	public CarDTO removeCar(long id) throws CarServiceException;
 
+	public CarDTO updateCar(long id, Car car)throws CarServiceException;
+
+	public CarDTO getCar(long id) throws CarServiceException;
+
+	public List<CarDTO> getAllCars() throws CarServiceException;
+
+	public List<CarDTO> getCarsByModel(String model) throws CarServiceException;
+
+	public List<CarDTO> getCarsByBrand(String brand)  throws CarServiceException;
 	
+	public List<CarDTO> getCarsByLocation(String registrationState) throws CarServiceException;
 
 }
