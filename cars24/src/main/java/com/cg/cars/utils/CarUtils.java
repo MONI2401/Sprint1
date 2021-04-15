5704da4bb6ebe36c49cefd8e58d498cf9e74fe99
Dhivya2907 <81764071+Dhivya2907@users.noreply.github.com>
2021-04-15 10:51:04 +0530

    Update CarUtils.java

--- cars24/src/main/java/com/cg/cars/utils/CarUtils.java
+++ cars24/src/main/java/com/cg/cars/utils/CarUtils.java
@@ -6,38 +6,38 @@ import java.util.List;
 import com.cg.cars.entities.Car;
 import com.cg.cars.model.CarDTO;
 
-
 public class CarUtils {
-	public static List<CarDTO> convertToCarDtoList(List<Car> list)
-	{
+	public static List<CarDTO> convertToCarDtoList(List<Car> list) {
 		List<CarDTO> CarDTOList = new ArrayList<CarDTO>();
 		for (Car car : list)
 			CarDTOList.add(convertToCarDto(car));
-	    return CarDTOList;
+		return CarDTOList;
 	}
-	
+
 	public static Car convertToCar(CarDTO CarDTO) {
 		Car Car = new Car();
-			
+
 		Car.setCarId(CarDTO.getCarId());
-		Car.setModel(Car.getModel());
-		Car.setBrand(Car.getBrand());
-		Car.setRegistrationState(Car.getRegistrationState());
-		Car.setRegistrationYear(Car.getRegistrationYear());
-		Car.setVariant(Car.getVariant());
-		
+		Car.setModel(CarDTO.getModel());
+		Car.setBrand(CarDTO.getBrand());
+		Car.setRegistrationState(CarDTO.getRegistrationState());
+		Car.setRegistrationYear(CarDTO.getRegistrationYear());
+		Car.setVariant(CarDTO.getVariant());
+		Car.setCustomers(CarDTO.getCustomer());
+
 		return Car;
 	}
-	
+
 	public static CarDTO convertToCarDto(Car Car) {
 		CarDTO CarDTO = new CarDTO();
-		
+
 		CarDTO.setCarId(Car.getCarId());
 		CarDTO.setBrand(Car.getBrand());
 		CarDTO.setModel(Car.getModel());
 		CarDTO.setVariant(Car.getVariant());
 		CarDTO.setRegistrationState(Car.getRegistrationState());
 		CarDTO.setRegistrationYear(Car.getRegistrationYear());
+		CarDTO.setCustomer(Car.getCustomers());
 		return CarDTO;
 	}
 
